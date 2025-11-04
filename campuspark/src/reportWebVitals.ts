5765ea608bd5c57f0a4eaf6a54b6b2f337455dc7
Priscillanne <priscillasankar1910@gmail.com>
2025-11-04 19:30:13 +0800

    Add reportWebVitals function for performance metrics

---
+++ campuspark/src/reportWebVitals.ts
@@ -0,0 +1,15 @@
+const reportWebVitals = (onPerfEntry?: (metric: any) => void) => {
+  if (onPerfEntry && typeof onPerfEntry === 'function') {
+    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+      getCLS(onPerfEntry);
+      getFID(onPerfEntry);
+      getFCP(onPerfEntry);
+      getLCP(onPerfEntry);
+      getTTFB(onPerfEntry);
+    }).catch(() => {
+      // ignore if web-vitals is not installed
+    });
+  }
+};
+
+export default reportWebVitals;
